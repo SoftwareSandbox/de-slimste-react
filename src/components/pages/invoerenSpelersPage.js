56ca2cfb8727a35d452454e9bb05c27d36fbfca2
Sch3lp <sch3lp@gmail.com>
2016-03-18 11:03:47 +0100

    for i loopke sukt jonges

--- src/components/pages/invoerenSpelersPage.js
+++ src/components/pages/invoerenSpelersPage.js
@@ -10,15 +10,9 @@ class InvoerenSpelersPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            spelers: ['', '', ''],
-            aangepast: false
+            spelers: [],
+            errors: []
         };
-    }
-
-    static willTransitionFrom(transition, component) {
-        if(component.state.aangepast && !confirm('De pagina verlaten zonder spelers op te slaan?')) {
-            transition.abort();
-        }
     }
 
     setSpelersState = (event) => {
@@ -28,18 +22,15 @@ class InvoerenSpelersPage extends Component {
         let waardeX = this.state.spelers[veld];
         let x = 0;
         this.setState({
-            spelers: [this.state.spelers[0], this.state.spelers[1], this.state.spelers[2]],
-            aangepast: true
+            spelers: [this.state.spelers[0], this.state.spelers[1], this.state.spelers[2]]
         });
     };
-
 
     bewaarSpelers = (event) => {
         event.preventDefault();
         if(this.spelerNamenGeldig()) { //TODO Aaron: spelers opslaan in store
             SpelerAction.createSpelers(this.state.spelers);
             Toastr.success('spelers opgeslagen');
-            this.setState({aangepast: false});
             // the router is now built on reactjs/history, and it is a first class API in the router for navigating
             browserHistory.push('driezesnegen');
         }
@@ -47,11 +38,11 @@ class InvoerenSpelersPage extends Component {
 
     spelerNamenGeldig() {
         let geldigeNamen = true;
-        this.state.errors = ['', '', ''];
+        this.state.errors = [];
 
-        for (let i = 0; i < this.state.spelers.length; i++) {
-            if(this.state.spelers[i].length === 0) {
-                this.state.errors[i] = "Geen lege naam toegestaan";
+        for (let spelerIndex in this.state.spelers) {
+            if(!this.state.spelers[spelerIndex]) {
+                this.state.errors[spelerIndex] = "Geen lege naam toegestaan";
                 geldigeNamen = false;
             }
         }
@@ -65,35 +56,35 @@ class InvoerenSpelersPage extends Component {
             <div>
                 <h1>Voer de namen van de spelers in!</h1>
                 <form>
-                <TextInput
-                    name="0"
-                    value={this.state.spelers[0]}
-                    onChange={this.setSpelersState}
-                    placeholder="Speler 1"
-                    error=''//{this.state.errors.speler1}
-                    af={true}
-                />
+                    <TextInput
+                        name="0"
+                        value={this.state.spelers[0]}
+                        onChange={this.setSpelersState}
+                        placeholder="Speler 1"
+                        error={this.state.errors[0]}
+                        autoFocus={true}
+                    />
 
-                <TextInput
-                    name="1"
-                    value={this.state.spelers[1]}
-                    onChange={this.setSpelersState}
-                    placeholder="Speler 2"
-                    error=''//{this.state.errors.speler2}
-                    af={true}
-                />
+                    <TextInput
+                        name="1"
+                        value={this.state.spelers[1]}
+                        onChange={this.setSpelersState}
+                        placeholder="Speler 2"
+                        error={this.state.errors[1]}
+                        autoFocus={false}
+                    />
 
-                <TextInput
-                    name="2"
-                    value={this.state.spelers[2]}
-                    onChange={this.setSpelersState}
-                    placeholder="Speler 3"
-                    error=''//{this.state.errors.speler3}
-                    af={true}
-                />
+                    <TextInput
+                        name="2"
+                        value={this.state.spelers[2]}
+                        onChange={this.setSpelersState}
+                        placeholder="Speler 3"
+                        error={this.state.errors[2]}
+                        autoFocus={false}
+                    />
 
-                <input type="button" value="Start de quiz" className="slimsteQuizConfiguratie" onClick={this.bewaarSpelers}/>
-            </form>
+                    <input type="button" value="Start de quiz" className="slimsteQuizConfiguratie" onClick={this.bewaarSpelers}/>
+                </form>
             </div>
         );
     }
